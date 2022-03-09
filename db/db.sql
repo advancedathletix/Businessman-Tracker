2c76ad8b29831c41b9bd39113d7bb4b0ba9d9c54
Elijah Walker <advancedathletix@gmail.com>
2022-03-09 09:47:36 -0500

    added in folders and files also added data base and first table

---
+++ db/db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE BusinessTrack;
+USE electioBusinessTrack;
---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL,
+
+  
